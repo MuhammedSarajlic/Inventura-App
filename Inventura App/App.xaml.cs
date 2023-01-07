e6d63d07c2afa32b16333a7267cc96f8d360676e
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-01-07 23:59:13 +0100

    Add project files.

---
+++ Inventura App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Inventura_App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Inventura App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Inventura_App;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
