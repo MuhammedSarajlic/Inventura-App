542a9506409c81cc72147f72fd23b27c438dd3bf
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-01-10 23:55:45 +0100

    Install SQLite, connect to db, add User model

---
+++ Inventura App/Data/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventura_App.Data
+{
+    internal class Database
+    {
+        public const string DatabaseFilename = "user.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            SQLite.SQLiteOpenFlags.Create |
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath => Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+    }
+}
---
+++ Inventura App/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Inventura_App.Models
+{
+    [Table("users")]
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [MaxLength(250), Unique]
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
