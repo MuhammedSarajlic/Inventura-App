c44cb84e3cd613eab77cb13643fc903ea622994f
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-01-21 21:11:54 +0100

    Feat: Add Welcome page (Carousel View)
    -Add three carousel page
    -Add on every page title, image, subtitle and paragraph

---
+++ Inventura App/WelcomePage.xaml.cs
@@ -0,0 +1,25 @@
+namespace Inventura_App;
+
+public partial class WelcomePage : ContentPage
+{
+    public class WelcomeMessage
+    {
+        public string Title { get; set; }
+        public string Photo { get; set; }
+        public string Subtitle { get; set; }
+        public string Paragraph { get; set; }
+    }
+
+    public List<WelcomeMessage> WelcomeMessages { get; set; }
+    public WelcomePage()
+	{
+        WelcomeMessages = new List<WelcomeMessage>()
+        {
+            new WelcomeMessage { Title = "Welcome to Stock Management", Photo = "welcome1.png", Subtitle = "Streamline your inventory", Paragraph = "Get started today" },
+            new WelcomeMessage { Title = "Manage Your Stock", Photo = "welcome2.png", Subtitle = "Efficiently and Effectively", Paragraph = "Start managing your stock today" },
+            new WelcomeMessage { Title = "Get Started", Photo = "welcome3.png", Subtitle = "Take control of your inventory", Paragraph = "Join us now" },
+        };
+        this.BindingContext = this;
+        InitializeComponent();
+    }
+}
