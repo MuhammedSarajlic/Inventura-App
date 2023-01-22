f5f4159fdc69fb4adfa41edd91cb6d73f8f5c9ed
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-01-22 16:19:13 +0100

    feat: Add button on last carousel page

--- Inventura App/WelcomePage.xaml.cs
+++ Inventura App/WelcomePage.xaml.cs
@@ -8,6 +8,7 @@ public partial class WelcomePage : ContentPage
         public string Photo { get; set; }
         public string Subtitle { get; set; }
         public string Paragraph { get; set; }
+        public bool Visible { get; set; }
     }
 
     public List<WelcomeMessage> WelcomeMessages { get; set; }
@@ -18,9 +19,9 @@ public partial class WelcomePage : ContentPage
         CurrentPage = 0;
         WelcomeMessages = new List<WelcomeMessage>()
         {
-            new WelcomeMessage { Title = "Welcome to Stock Management", Photo = "welcome1.png", Subtitle = "Streamline your inventory", Paragraph = "Get started today" },
-            new WelcomeMessage { Title = "Manage Your Stock", Photo = "welcome2.png", Subtitle = "Efficiently and Effectively", Paragraph = "Start managing your stock today" },
-            new WelcomeMessage { Title = "Get Started", Photo = "welcome3.png", Subtitle = "Take control of your inventory", Paragraph = "Join us now" },
+            new WelcomeMessage { Title = "Welcome to Stock Management", Photo = "welcome1.png", Subtitle = "Streamline your inventory", Paragraph = "Get started today", Visible = false },
+            new WelcomeMessage { Title = "Manage Your Stock", Photo = "welcome2.png", Subtitle = "Efficiently and Effectively", Paragraph = "Start managing your stock today", Visible = false },
+            new WelcomeMessage { Title = "Get Started", Photo = "welcome3.png", Subtitle = "Take control of your inventory", Paragraph = "Join us now", Visible = true },
         };
         this.BindingContext = this;
         InitializeComponent();
