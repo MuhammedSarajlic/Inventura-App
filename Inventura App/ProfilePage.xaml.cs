9543a3f2cec04b37ca6713bf6aece9c697c6cae7
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-01-25 02:00:55 +0100

    feat: Add profile page

--- Inventura App/ProfilePage.xaml.cs
+++ Inventura App/ProfilePage.xaml.cs
@@ -1,9 +1,28 @@
+using Inventura_App.Data;
+using Inventura_App.Models;
+using SQLite;
+
 namespace Inventura_App;
 
 public partial class ProfilePage : ContentPage
 {
-	public ProfilePage()
-	{
-		InitializeComponent();
-	}
+    private SQLiteAsyncConnection _connection;
+    
+    public ProfilePage()
+    {
+        InitializeComponent();
+        _connection = new SQLiteAsyncConnection(Data.Database.DatabasePath, Data.Database.Flags);
+        var email = UserCred.Email;
+        var query = _connection.Table<User>().Where(u => u.Email == email);
+        var user = query.FirstOrDefaultAsync();
+        user.ContinueWith(t =>
+        {
+            if (t.Result != null)
+            {
+                BindingContext = t.Result;
+                nameLabel.BindingContext = BindingContext;
+                emailLabel.BindingContext = BindingContext;
+            }
+        });
+    }
 }
