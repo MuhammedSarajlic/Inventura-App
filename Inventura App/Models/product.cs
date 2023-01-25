ade955f0cafce378600782a16b83f76b025b6933
Faris <doricfaris50@gmail.com>
2023-01-25 20:32:16 +0100

    Add product model and button

---
+++ Inventura App/Models/product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Inventura_App.Models
+{
+
+    [Table("products")]
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+
+    }
+}
