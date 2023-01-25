0c232311515c3cfbc3e67b4d387005681c013e98
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-01-25 23:32:47 +0100

    feat: Get all products from db
    -Get all products from db and product count when you open product page

--- Inventura App/ProductsPage.xaml.cs
+++ Inventura App/ProductsPage.xaml.cs
@@ -1,9 +1,39 @@
+using Inventura_App.Models;
+using SQLite;
+using System.Collections.ObjectModel;
+
 namespace Inventura_App;
 
 public partial class ProductsPage : ContentPage
 {
-	public ProductsPage()
-	{
-		InitializeComponent();
-	}
+    private SQLiteAsyncConnection _connection;
+    public ObservableCollection<Product> Products { get; set; }
+
+    public ProductsPage()
+    {
+        InitializeComponent();
+        _connection = new SQLiteAsyncConnection(Data.Database.DatabasePath, Data.Database.Flags);
+        Products = new ObservableCollection<Product>();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var numberOfProducts = await _connection.Table<Product>().CountAsync();
+        totalProducts.Text = numberOfProducts.ToString();
+        var products = await _connection.Table<Product>().ToListAsync();
+        Products.Clear();
+        foreach (var product in products)
+        {
+            Products.Add(product);
+        }
+        productListView.ItemsSource = Products;
+
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new AddProductPage());
+    }
+
 }
