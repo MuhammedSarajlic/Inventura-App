c57f13626182eb56e3f1699c5973988be3322e4b
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-01-25 01:57:54 +0100

    feat: Add current user email variable globally

---
+++ Inventura App/Data/UserCred.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventura_App.Data
+{
+    public static class UserCred
+    {
+        public static string Email { get; set; }
+    }
+}
