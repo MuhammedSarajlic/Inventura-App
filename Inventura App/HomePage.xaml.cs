0ae4508af9bdf1c2ca2aaecfad546b44627cc0af
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-01-26 00:23:28 +0100

    feat: Display products count
    -Remove hardcoded values
    -Add total products and items in stock values

--- Inventura App/HomePage.xaml.cs
+++ Inventura App/HomePage.xaml.cs
@@ -1,10 +1,31 @@
+using AndroidX.Lifecycle;
+using Inventura_App.Models;
+using SQLite;
+
 namespace Inventura_App;
 
 public partial class HomePage : ContentPage
 {
+    public int TotalProducts { get; set; }
+    public int ItemsInStock { get; set; }
+    private SQLiteAsyncConnection _connection;
     public HomePage()
 	{
 		InitializeComponent();
+        _connection = new SQLiteAsyncConnection(Data.Database.DatabasePath, Data.Database.Flags);
+        
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var count = await _connection.ExecuteScalarAsync<int>("SELECT COUNT(*) FROM products");
+        var uniqueCount = await _connection.ExecuteScalarAsync<int>("SELECT COUNT(DISTINCT name) FROM products");
+        TotalProducts = uniqueCount;
+        ItemsInStock = count;
+        totalProducts.Text = TotalProducts.ToString();
+        itemsInStock.Text = ItemsInStock.ToString();
+        BindingContext = this;
 
     }
 }
