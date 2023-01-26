e7936095d91a82b593435720b4bba334b6e8e01f
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-01-26 14:11:34 +0100

    feat: Add selected product data globally

---
+++ Inventura App/Data/ProductItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventura_App.Models;
+
+namespace Inventura_App.Data
+{
+    public static class ProductItem
+    {
+        public static Product SelectedProduct { get; set; }
+    }
+}
