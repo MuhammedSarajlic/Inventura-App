821d14cef2dcf7d03e4a018dcf8258afbcee80d2
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-01-26 14:16:30 +0100

    feat: Current product class
    -Create current product class
    -Store selected product data in this class
    -Add current product class to binding context

---
+++ Inventura App/ProductDetailsPage.xaml.cs
@@ -0,0 +1,33 @@
+using Inventura_App.Data;
+using static Inventura_App.WelcomePage;
+using static Java.Util.Jar.Attributes;
+using Inventura_App.Models;
+
+namespace Inventura_App;
+
+public partial class ProductDetailsPage : ContentPage
+{
+	public class CurrentProduct
+	{
+		public string Name { get; set; }
+		public string Category { get; set; }
+		public string Location { get; set; }
+		public string Description { get; set; }
+		public string Date { get; set; }
+		
+	}
+	public CurrentProduct Item { get; set; }
+	public ProductDetailsPage()
+	{
+        Item = new CurrentProduct
+		{
+			Name = ProductItem.SelectedProduct.Name,
+			Category = ProductItem.SelectedProduct.Category,
+			Location = ProductItem.SelectedProduct.Location,
+			Description = ProductItem.SelectedProduct.Description,
+			Date = ProductItem.SelectedProduct.Date
+        };
+        this.BindingContext = Item;
+		InitializeComponent();
+    }
+}
