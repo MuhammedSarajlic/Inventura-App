35c7652e3d4c3f466ffadcf548833d8604f40d1d
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-01-26 16:35:53 +0100

    feat: Change chart frame height
    -Get current day and change frame height with this name
    -Get height by multiplying count of items with 10

--- Inventura App/HomePage.xaml.cs
+++ Inventura App/HomePage.xaml.cs
@@ -8,12 +8,17 @@ public partial class HomePage : ContentPage
 {
     public int TotalProducts { get; set; }
     public int ItemsInStock { get; set; }
+    public double Height { get; set; }
+
+    public string CurrentDay { get; set; }
+
     private SQLiteAsyncConnection _connection;
     public HomePage()
 	{
 		InitializeComponent();
         _connection = new SQLiteAsyncConnection(Data.Database.DatabasePath, Data.Database.Flags);
-        
+        CurrentDay = DateTime.Today.DayOfWeek.ToString();
+        BindingContext = this;
     }
 
     protected override async void OnAppearing()
@@ -25,6 +30,38 @@ public partial class HomePage : ContentPage
         ItemsInStock = count;
         totalProducts.Text = TotalProducts.ToString();
         itemsInStock.Text = ItemsInStock.ToString();
+        Height = (count * 10) + 20;
+        if(CurrentDay == "Monday")
+        {
+            Monday.HeightRequest = Height;
+        }else if(CurrentDay == "Tuesday")
+        {
+            Tuesday.HeightRequest = Height;
+        }
+        else if (CurrentDay == "Wednesday")
+        {
+            Wednesday.HeightRequest = Height;
+        }
+        else if (CurrentDay == "Thursday")
+        {
+            Thursday.HeightRequest = Height;
+        }
+        else if (CurrentDay == "Friday")
+        {
+            Friday.HeightRequest = Height;
+        }
+        else if (CurrentDay == "Saturday")
+        {
+            Saturday.HeightRequest = Height;
+        }
+        else if (CurrentDay == "Sunday")
+        {
+            Sunday.HeightRequest = Height;
+        }
+        else
+        {
+            Height = 0;
+        }
         BindingContext = this;
 
     }
